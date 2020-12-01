e8649f49711d433a46223c7717c2666cf8c76c65
Tobi <tobi-git@grimm.gr>
2020-12-01 09:14:00 +0100

    part2

--- day01/day01_part2.py
+++ day01/day01_part2.py
@@ -4,11 +4,11 @@ from operator import mul
 from functools import reduce
 from itertools import combinations
 
-def calc_2020(inlist: List[int]) -> Tuple[int, int]:
+def calc_2020(inlist: List[int], num: int = 2) -> Tuple[int, int]:
     """compare all numbers in inlist with each other.
        If the sum of two numbers results in 2020,
        return the first two numbers as a tuple"""
-    return [(i, j) for i, j in combinations(inlist, 2) if i+j == 2020][0]
+    return [(items) for items in combinations(inlist, num) if sum(items) == 2020][0]
 
 
 
@@ -19,3 +19,4 @@ if __name__ == "__main__":
         # important: input should contain no empty lines, otherwise the int() cast will fail
         inlist = [int(line.strip("\r\n").strip()) for line in infile]
     print("Product of the two numbers for day01 - part 1:", reduce(mul, calc_2020(inlist), 1))
+    print("Product of the three numbers for day01 - part 2:", reduce(mul, calc_2020(inlist, 3), 1))
