7889db2d1b5ee1750ba41bf270f0f06198020bb3
Tobi <tobi-git@grimm.gr>
2020-12-01 11:34:37 +0100

    nicer formatting

--- day01/day01_part1.py
+++ day01/day01_part1.py
@@ -4,18 +4,20 @@ from operator import mul
 from functools import reduce
 from itertools import combinations
 
+
 def calc_2020(inlist: List[int]) -> Tuple[int, int]:
     """compare all numbers in inlist with each other.
-       If the sum of two numbers results in 2020,
-       return the first two numbers as a tuple"""
-    return [(i, j) for i, j in combinations(inlist, 2) if i+j == 2020][0]
-
-
+    If the sum of two numbers results in 2020,
+    return the first two numbers as a tuple"""
+    return [(i, j) for i, j in combinations(inlist, 2) if i + j == 2020][0]
 
 
 if __name__ == "__main__":
 
-    with open("input", 'r') as infile:
+    with open("input", "r") as infile:
         # important: input should contain no empty lines, otherwise the int() cast will fail
         inlist = [int(line.strip("\r\n").strip()) for line in infile]
-    print("Product of the two numbers for day01 - part 1:", reduce(mul, calc_2020(inlist), 1))
+    print(
+        "Product of the two numbers for day01 - part 1:",
+        reduce(mul, calc_2020(inlist), 1),
+    )
--- day01/day01_part2.py
+++ day01/day01_part2.py
@@ -4,19 +4,24 @@ from operator import mul
 from functools import reduce
 from itertools import combinations
 
+
 def calc_2020(inlist: List[int], num: int = 2) -> Tuple[int, ...]:
     """compare all numbers in inlist with each other.
-       If the sum of two numbers results in 2020,
-       return the first two numbers as a tuple"""
+    If the sum of two numbers results in 2020,
+    return the first two numbers as a tuple"""
     return [(items) for items in combinations(inlist, num) if sum(items) == 2020][0]
-
-
 
 
 if __name__ == "__main__":
 
-    with open("input", 'r') as infile:
+    with open("input", "r") as infile:
         # important: input should contain no empty lines, otherwise the int() cast will fail
         inlist = [int(line.strip("\r\n").strip()) for line in infile]
-    print("Product of the two numbers for day01 - part 1:", reduce(mul, calc_2020(inlist), 1))
-    print("Product of the three numbers for day01 - part 2:", reduce(mul, calc_2020(inlist, 3), 1))
+    print(
+        "Product of the two numbers for day01 - part 1:",
+        reduce(mul, calc_2020(inlist), 1),
+    )
+    print(
+        "Product of the three numbers for day01 - part 2:",
+        reduce(mul, calc_2020(inlist, 3), 1),
+    )
