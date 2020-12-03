15a327fe329265c2144fb2220381a556fb6f1ffe
Tobi <tobi-git@grimm.gr>
2020-12-03 06:43:33 +0100

    part 2 done

--- day03/day03.py
+++ day03/day03.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import sys
 from typing import List
+from operator import mul
+from functools import reduce
 
 
-def navigate_slope(map: List, x_inc: int = 1, y_inc: int = 3) -> int:
+def navigate_slope(map: List[str], x_inc: int = 1, y_inc: int = 3) -> int:
     h = len(map)
     w = len(map[0])
     x, y = 0, 0
@@ -22,4 +24,11 @@ def navigate_slope(map: List, x_inc: int = 1, y_inc: int = 3) -> int:
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as infile:
         map = [line.strip() for line in infile]
-    print(f"part 1: {navigate_slope(map)} trees hit")
+    part1 = navigate_slope(map)
+    print(f"part 1: {part1} trees hit")
+    results = []
+    for x,y in [(1,1), (1,3), (1,5), (1,7), (2,1)]:
+        results.append(navigate_slope(map, x, y))
+
+    print("part 2: ", reduce(mul, results, 1), " trees hit")
+    
