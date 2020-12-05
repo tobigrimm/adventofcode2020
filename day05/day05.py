e20cc5df0a2f2801d73119347b770d0eba4435e1
Tobi <tobi-git@grimm.gr>
2020-12-05 14:55:11 +0100

    Day 5: part 2 done, but typing missing :)

--- day05/day05.py
+++ day05/day05.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import sys
 import re
-from typing import Dict, List
-
+from typing import Dict, List, Iterator, Generator, Any 
+from itertools import islice
 
 def calculate_bsp(code: str, subrange: List[int]) -> List[int]:
     for char in code:
@@ -12,19 +12,41 @@ def calculate_bsp(code: str, subrange: List[int]) -> List[int]:
             subrange = subrange[int(len(subrange) / 2) :]
     return subrange
 
+def window(seq: Iterator[Any], n: int =2) -> Generator[Any, None, None]:
+    "Returns a sliding window (of width n) over data from the iterable"
+    "   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   "
+    "taken from https://docs.python.org/release/2.3.5/lib/itertools-example.html"
+    it = iter(seq)
+    result = tuple(islice(it, n))
+    if len(result) == n:
+        yield result
+    for elem in it:
+        result = result[1:] + (elem,)
+        yield result
 
 def calculate_seat(seat: str) -> int:
     column = calculate_bsp(seat[:7], list(range(128)))[0]
     row = calculate_bsp(seat[7:], list(range(8)))[0]
-    print(column, row)
     return column * 8 + row
 
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as infile:
         seatcodes = [nextline.strip() for nextline in infile]
+    all_seats = sorted([calculate_seat(seat) for seat in seatcodes])
     print(
         "part 1: ",
-        max([calculate_seat(seat) for seat in seatcodes]),
+        max(all_seats),
         " is the highest seat code",
+    )
+    for i,j,k in window(all_seats, 3):
+        if i == j-1 and j+1 == k:
+            pass
+        else:
+            if j+1 != k:
+                missing_seat  = j+1
+    print(
+        "part 2: ",
+        missing_seat,
+        " is missing in the seat codes",
     )
