201e1a99783488cc81cfef78a16f808b711d76a2
Tobi <tobi-git@grimm.gr>
2020-12-07 08:50:41 +0100

    added input, proper escaping for the regex search string

--- day07/day07.py
+++ day07/day07.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 import sys
 import re
-from typing import List, Set
+from typing import Any
 import networkx as nx
 
-def count_bags(graph, node):
+
+def count_bags(graph: Any, node: str) -> int:
     number = 1
     for subnode, amount in G[node].items():
         subnum = count_bags(G, subnode)
-        number += amount['num'] * subnum
+        number += amount["num"] * subnum
     return number
+
 
 if __name__ == "__main__":
     G = nx.DiGraph()
     with open(sys.argv[1], "r") as infile:
         for nextline in infile:
-            search = "(?P<amount>[0-9])+\ (?P<color>[a-z]*\ [a-z]*)\ bag[s]?"
+            search = r"(?P<amount>[0-9])+\ (?P<color>[a-z]*\ [a-z]*)\ bag[s]?"
             left, right = nextline.strip().split("bags contain")
             left = left.strip()
             right = right.strip()
@@ -26,6 +28,6 @@ if __name__ == "__main__":
                 color = m.group("color")
                 G.add_edge(left, color, num=int(amount))
 
-    print("Part 1: ", len(nx.ancestors(G, 'shiny gold')))
+    print("Part 1: ", len(nx.ancestors(G, "shiny gold")))
     # dont count the topmost shiny gold bag!
-    print("Part 2: ", count_bags(G, 'shiny gold')-1)
+    print("Part 2: ", count_bags(G, "shiny gold") - 1)
