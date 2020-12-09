682c28ababd0e64954e00818f0a3d484f1e8dba7
Tobi <tobi-git@grimm.gr>
2020-12-09 11:04:56 +0100

    Day 9: Part 2 done

--- day09/day09.py
+++ day09/day09.py
@@ -4,6 +4,7 @@ import re
 from itertools import islice, permutations
 
 from typing import Any, List, Iterator, Generator
+
 # from ..tools import sliding_window
 
 
@@ -20,12 +21,24 @@ def window(seq: Iterator[Any], n: int = 2) -> Generator[Any, None, None]:
         yield result
 
 
+def check_continous(numbers: List[int], target: int) -> int:
+    # check increasing sliding windows, sum needs to be target
+    for i in range(2, len(numbers)):
+        for slidewin in window(numbers, i):
+            if sum(slidewin) == target:
+                print("slide:", slidewin)
+                return int(min(slidewin) + max(slidewin))
+
+    return 0
+
+
 def check_sums(numbers: List[int], preamble_len: int) -> int:
-    for *sublist, tail in window(numbers, preamble_len+1):
+    for *sublist, tail in window(numbers, preamble_len + 1):
         results = [sum(perm) == tail for perm in permutations(sublist, 2)]
         if not any(results):
-            return tail
+            return int(tail)
     return -1
+
 
 if __name__ == "__main__":
     preamble_len = int(sys.argv[2])
@@ -34,5 +47,9 @@ if __name__ == "__main__":
         for nextline in infile:
             num = int(nextline.strip())
             numbers.append(num)
-    print("Part 1:", check_sums(numbers, preamble_len), " is not the sum of the preceding %s numbers" % preamble_len)
-    print("Part 2:")
+    target = check_sums(numbers, preamble_len)
+    print(
+        "Part 1:", target, " is not the sum of the preceding %s numbers" % preamble_len
+    )
+    part2_sum = check_continous(numbers[: numbers.index(target)], target)
+    print("Part 2:", part2_sum)
