5172c764af0f7e9479923f654bcadbecf18ba116
Tobi <tobi-git@grimm.gr>
2020-12-10 06:31:00 +0100

    Day 10: make flake8 happy :)

--- day10/day10.py
+++ day10/day10.py
@@ -1,50 +1,34 @@
 #!/usr/bin/env python3
 import sys
-import re
-from itertools import islice, permutations
 from collections import Counter, defaultdict
-from typing import Any, List, Iterator, Generator
-
-# from ..tools import sliding_window
+from typing import List, Counter as TypeCounter
 
 
-def window(seq: Iterator[Any], n: int = 2) -> Generator[Any, None, None]:
-    "Returns a sliding window (of width n) over data from the iterable"
-    "   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   "
-    "taken from https://docs.python.org/release/2.3.5/lib/itertools-example.html"
-    it = iter(seq)
-    result = tuple(islice(it, n))
-    if len(result) == n:
-        yield result
-    for elem in it:
-        result = result[1:] + (elem,)
-        yield result
-
-
-def calculate_diffs(inlist) -> int:
-    result = Counter()
+def calculate_diffs(inlist: List[int]) -> int:
+    result: TypeCounter[int] = Counter()
     jolt = 0
     dist = 0
     for i in inlist:
-    
+
         diff = i - jolt
         if diff == 1:
-            dist+=1
-            result[diff]+=1
+            dist += 1
+            result[diff] += 1
         jolt = i
 
     # add final 3 jolt jump
-    result[3]+=1
-    return int(result[1]*result[3])
+    result[3] += 1
+    return int(result[1] * result[3])
 
-def get_variants(inlist) -> int:
+
+def get_variants(inlist: List[int]) -> int:
     # defaultdict from int will default to 0 if an entry does not exist
     ways = defaultdict(int)
     ways[0] = 1
-    # skip the leading 0, as that is always reached 
+    # skip the leading 0, as that is always reached
     # and already account for above :)
     for i in inlist[1:]:
-        ways[i] = ways[i-1]+ways[i-2]+ways[i-3]
+        ways[i] = ways[i - 1] + ways[i - 2] + ways[i - 3]
     return ways[max(inlist)]
 
 
